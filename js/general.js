c2935c11db27a88041baaeaaf346229a8ea96d7c
marconip <marconidesenhos@gmail.com>
2020-07-21 11:29:11 -0300

    ajustes

--- js/general.js
+++ js/general.js
@@ -2,3 +2,19 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
+document.querySelector(".darkmode").onclick = function () {
+  var dashboard = document.querySelector("body");
+  var onoff = document.querySelector(".darkmode p");
+  if (dashboard.classList.contains("dark-mode") || onoff.classList.contains("text-success")) {//DARK
+    dashboard.classList.remove("dark-mode");
+    onoff.classList.remove("text-success");
+    onoff.classList.add("text-danger");
+    onoff.innerHTML = "Off";
+  } else {//LIGHT
+    dashboard.classList.add("dark-mode");
+    onoff.classList.remove("text-danger");
+    onoff.classList.add("text-success");
+    onoff.innerHTML = "On";
+  }
+
+};
