ac1304607a5af1f3eba52bc459a6b18d5d02377a
marconip <marconidesenhos@gmail.com>
2020-07-24 17:13:31 -0300

    more

--- js/general.js
+++ js/general.js
@@ -19,12 +19,23 @@ document.querySelector(".darkmode").onclick = function () {
   }
 };
 
+//textos desenvolvido por
+document.querySelector(".desenvolvimento-mobile").onclick = function () {
+  document.querySelector(".desenvolvimento").classList.toggle('clicado');
+};
+
+//menu icone abrir fechar
+document.querySelector(".menu-icone").onclick = function () {
+  this.classList.toggle('clicado');
+  document.querySelector("aside").classList.toggle('menu-fechado');
+  document.querySelector(".desenvolvimento").classList.remove('clicado');
+}
+
 //menu
 var menu = document.querySelectorAll("aside li");
 menu.forEach(function (el) {
   el.onclick = function () {
-    var menus = document.querySelectorAll("aside li");
-    menus.forEach(function (all) {
+    document.querySelectorAll("aside li").forEach(function (all) {
       all.classList.remove("active");
     });
     if (el.classList.contains("active")) {
@@ -41,15 +52,12 @@ window.onresize = function () {
   resolucao();
 };
 function resolucao(x) {
-  if (window.matchMedia("(max-width: 991px)").matches) {
-    var submenu = document.querySelectorAll("aside li a");
-    submenu.forEach(function (el) {
-      el.removeAttribute("data-toggle");
-      if (el.nextElementSibling !== null) {
-        el.nextElementSibling.classList.add("show");
-      }
-    })
-  } else {
+  if (window.matchMedia("(min-width: 992px)").matches) {
+
+    document.querySelector("aside").classList.remove('menu-phone');
+    document.querySelector("aside").classList.remove('menu-fechado');
+    document.querySelector(".menu-icone").classList.remove("clicado");
+
     var submenu = document.querySelectorAll("aside li a");
     submenu.forEach(function (el) {
       el.setAttribute("data-toggle", "collapse");
@@ -57,6 +65,28 @@ function resolucao(x) {
         el.nextElementSibling.classList.remove("show");
       }
     })
+  };
+
+  if (window.matchMedia("(max-width: 991px)").matches) {
+
+    document.querySelector("aside").classList.remove('menu-phone');
+    document.querySelector(".desenvolvimento").classList.remove('clicado');
+    document.querySelector("aside").classList.add('menu-fechado');
+    document.querySelector(".menu-icone").classList.add("clicado");
+
+    var submenu = document.querySelectorAll("aside li a");
+    submenu.forEach(function (el) {
+      el.removeAttribute("data-toggle");
+      if (el.nextElementSibling !== null) {
+        el.nextElementSibling.classList.add("show");
+      }
+    })
+  };
+
+  if (window.matchMedia("(max-width: 575px)").matches) {
+    document.querySelector("aside").classList.remove('menu-fechado');
+    document.querySelector("aside").classList.add('menu-phone');
   }
+  
 };
 
